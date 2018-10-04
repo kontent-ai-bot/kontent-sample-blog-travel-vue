49d4bf955fe107790ed10718d135a58cc16a0e71
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2018-10-04 15:35:57 -0400

    Make home header dynamic

--- src/components/the-header-home.vue
+++ src/components/the-header-home.vue
@@ -1,14 +1,14 @@
 <template>
   <base-header :is-home="true">
       <h1 class="blog-name">
-        Blog name
+        {{name}}
       </h1>
       <span class="blog-description">
-        Blog description
+        {{description}}
       </span>
       <div
         class="blog-cover cover"
-        style="background-image: url('https://source.unsplash.com/random/1920x900')"
+        :style="`background-image: url('${coverImageUrl}')`"
       ></div>
   </base-header>
 </template>
@@ -20,6 +20,23 @@ export default {
   name: 'the-header-home',
   components: {
     BaseHeader
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: 'name'
+    },
+    description: {
+      type: String,
+      required: true,
+      default: 'description'
+    },
+    coverImageUrl: {
+      type: String,
+      required: true,
+      default: 'cover-image-url'
+    }
   }
 }
 </script>
