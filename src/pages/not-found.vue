6927b17936fd42cce03e5f8e8f3bcc89caf7c8fd
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2018-10-05 10:35:36 -0400

    Clean-up 404 page

--- src/pages/not-found.vue
+++ src/pages/not-found.vue
@@ -9,77 +9,13 @@
 
 <script>
 import BaseLayoutPage from '@/components/base-layout-page'
-import PostList from '@/components/post-list'
 import TheHeaderGeneric from '@/components/the-header-generic'
 
 export default {
-  name: 'activity-detail',
+  name: 'not-found',
   components: {
     BaseLayoutPage,
-    PostList,
     TheHeaderGeneric
-  },
-  data: function () {
-    return {
-      activity: {
-        name: 'Activity name',
-        slug: 'activity-slug'
-      },
-      posts: [
-        {
-          id: 1,
-          title: 'Post Title',
-          slug: 'post-slug',
-          excerpt: 'Post Excerpt',
-          published: new Date(Date.now()),
-          author: {
-            name: 'John Smith',
-            slug: 'john-smith'
-          },
-          activities: [
-            {
-              name: 'Hiking',
-              slug: 'hiking'
-            },
-            {
-              name: 'Sightseeing',
-              slug: 'sightseeing'
-            }
-          ]
-        }, {
-          id: 2,
-          title: 'Post Title',
-          slug: 'post-slug',
-          excerpt: 'Post Excerpt',
-          published: new Date(Date.now()),
-          author: {
-            name: 'John Smith',
-            slug: 'john-smith'
-          },
-          activities: [
-            {
-              name: 'Hiking',
-              slug: 'hiking'
-            }
-          ]
-        }, {
-          id: 3,
-          title: 'Post Title',
-          slug: 'post-slug',
-          excerpt: 'Post Excerpt',
-          published: new Date(Date.now()),
-          author: {
-            name: 'John Smith',
-            slug: 'john-smith'
-          },
-          activities: []
-        }]
-    }
-  },
-  computed: {
-    subtitle: function () {
-      return `All posts in ${this.activity.name.toLowerCase()}`
-    }
   }
 }
 </script>
