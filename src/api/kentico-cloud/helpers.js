415a029b9754834ef3308e0f2a3581caae853783
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2018-10-18 17:49:58 +0200

    Add tag flattener

--- src/api/kentico-cloud/helpers.js
+++ src/api/kentico-cloud/helpers.js
@@ -1,3 +1,14 @@
 export function getFirstAssetUrl (assets) {
   return assets && assets.length > 0 ? assets[0].url : ''
 }
+
+export function flattenTag (tag) {
+  return {
+    title: tag.name,
+    slug: slugifyCodename(tag.codename)
+  }
+}
+
+export function slugifyCodename (codename) {
+  return codename.replace(/_/g, '-')
+}
