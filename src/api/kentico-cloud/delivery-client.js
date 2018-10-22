51d2dcb3a184a1a8cdfcf10d9a7ca92d922a2373
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2018-10-22 17:10:29 +0200

    Change wrapper tag for linked items

--- src/api/kentico-cloud/delivery-client.js
+++ src/api/kentico-cloud/delivery-client.js
@@ -3,5 +3,8 @@ import { ProjectTypeResolvers } from './type-resolvers'
 
 export const deliveryClient = new DeliveryClient({
   projectId: process.env.VUE_APP_KENTICO_CLOUD_PROJECT_ID,
-  typeResolvers: ProjectTypeResolvers
+  typeResolvers: ProjectTypeResolvers,
+  linkedItemResolver: {
+    linkedItemWrapperTag: 'span'
+  }
 })
